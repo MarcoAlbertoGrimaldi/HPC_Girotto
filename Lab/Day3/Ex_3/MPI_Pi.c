118a97b6f7f1bbbf0a1cfcd169fe5bfc7cbb782e
Marco Alberto Grimaldi <marco.alberto.grimaldi@gmail.com>
2019-07-12 00:43:05 +0200

    Day3

--- Lab/Day3/Ex_3/MPI_Pi.c
+++ Lab/Day3/Ex_3/MPI_Pi.c
@@ -5,7 +5,7 @@
 #include <math.h>
 
 
-int main( int argc, char * argv[] ){
+int main(){
 
     const double a = 0;
     double const b = 1;
@@ -16,7 +16,7 @@ int main( int argc, char * argv[] ){
     int npes = 1; // store the number of MPI processes
     double x_j = 0;
 
-    MPI_Init( &argc, &argv );
+    MPI_Init(NULL, NULL);
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
     MPI_Comm_size( MPI_COMM_WORLD, &npes );
 
@@ -32,14 +32,22 @@ int main( int argc, char * argv[] ){
         approx += 1/(1 + x_j*x_j);
     }
 
-    MPI_Reduce(&approx, &res, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
     stop = MPI_Wtime();
 
-    if(rank==0){
+    MPI_Reduce(&approx, &res, 1, MPI_DOUBLE, MPI_SUM, npes-1, MPI_COMM_WORLD);
+
+    if(rank == npes-1){
+        MPI_Send(&res, 1, MPI_DOUBLE, 0, 101, MPI_COMM_WORLD);
+        fprintf( stderr, "Sending %f to process 0... \n", res);
+    }
+
+    if(rank == 0){
+        fprintf( stderr, "Waiting from process %i... \n", npes-1);
+        MPI_Recv(&res, 1, MPI_DOUBLE, npes-1, 101, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         res = res * 4 * h;
         fprintf( stderr, "Done in %f seconds, pi approx is: %f.\n", stop-start, res);
     }
+
     MPI_Finalize();
 
     return 0;
